444890d6e6844fbaeefe5715ba8d62ad86d3a300
EC2 Default User <ec2-user@ip-172-31-8-234.ap-northeast-1.compute.internal>
2020-09-06 22:00:44 +0000

    application.rbの設定を変更

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module AttendanceApp
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config','locales', '**', '*.{rb,yml}').to_s]
-    config.enable_dependency_loading = true
+    config.autoload_paths += %W(#{Rails.root}/lib)
+    config.eager_load_paths += %W(#{Rails.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
