b0fbca65699aab0bd7a0063882bf23f82475d32e
EC2 Default User <ec2-user@ip-172-31-8-234.ap-northeast-1.compute.internal>
2020-11-02 13:01:51 +0000

    予約可能時間のボタン表示を本日で現在時刻以前は表示しないよう修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,35 +14,65 @@ module ApplicationHelper
   # 引き数は(Userオブジェクト, Attendanceオブジェクト)
   def first_and_last_lesson_time(user, attendance)
     lesson_time_array = []
-    lesson_time_array.push(attendance.lesson_status_00)
-    lesson_time_array.push(attendance.lesson_status_01)
-    lesson_time_array.push(attendance.lesson_status_02)
-    lesson_time_array.push(attendance.lesson_status_03)
-    lesson_time_array.push(attendance.lesson_status_04)
-    lesson_time_array.push(attendance.lesson_status_05)
-    lesson_time_array.push(attendance.lesson_status_06)
-    lesson_time_array.push(attendance.lesson_status_07)
-    lesson_time_array.push(attendance.lesson_status_08)
-    lesson_time_array.push(attendance.lesson_status_09)
-    lesson_time_array.push(attendance.lesson_status_10)
-    lesson_time_array.push(attendance.lesson_status_11)
-    lesson_time_array.push(attendance.lesson_status_12)
-    lesson_time_array.push(attendance.lesson_status_13)
-    lesson_time_array.push(attendance.lesson_status_14)
-    lesson_time_array.push(attendance.lesson_status_15)
-    lesson_time_array.push(attendance.lesson_status_16)
-    lesson_time_array.push(attendance.lesson_status_17)
-    lesson_time_array.push(attendance.lesson_status_18)
-    lesson_time_array.push(attendance.lesson_status_19)
-    lesson_time_array.push(attendance.lesson_status_20)
-    lesson_time_array.push(attendance.lesson_status_21)
-    lesson_time_array.push(attendance.lesson_status_22)
-    lesson_time_array.push(attendance.lesson_status_23)
+    # もしattendanceが本日の日付だったら現在時刻以前はnilを入れる
+    if attendance.worked_on == Date.today
+      # lesson_time_array.push(attendance.lesson_status_00)
+      Time.current.hour >= 0 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_00)
+      Time.current.hour >= 1 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_01)
+      Time.current.hour >= 2 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_02)
+      Time.current.hour >= 3 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_03)
+      Time.current.hour >= 4 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_04)
+      Time.current.hour >= 5 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_05)
+      Time.current.hour >= 6 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_06)
+      Time.current.hour >= 7 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_07)
+      Time.current.hour >= 8 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_08)
+      Time.current.hour >= 9 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_09)
+      Time.current.hour >= 10 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_10)
+      Time.current.hour >= 11 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_11)
+      Time.current.hour >= 12 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_12)
+      Time.current.hour >= 13 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_13)
+      Time.current.hour >= 14 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_14)
+      Time.current.hour >= 15 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_15)
+      Time.current.hour >= 16 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_16)
+      Time.current.hour >= 17 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_17)
+      Time.current.hour >= 18 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_18)
+      Time.current.hour >= 19 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_19)
+      Time.current.hour >= 20 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_20)
+      Time.current.hour >= 21 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_21)
+      Time.current.hour >= 22 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_22)
+      Time.current.hour >= 23 ? lesson_time_array.push(nil) : lesson_time_array.push(attendance.lesson_status_23)
+    # もしattendanceが本日でなければ通常通りステータスを入れる
+    else
+      lesson_time_array.push(attendance.lesson_status_00)
+      lesson_time_array.push(attendance.lesson_status_01)
+      lesson_time_array.push(attendance.lesson_status_02)
+      lesson_time_array.push(attendance.lesson_status_03)
+      lesson_time_array.push(attendance.lesson_status_04)
+      lesson_time_array.push(attendance.lesson_status_05)
+      lesson_time_array.push(attendance.lesson_status_06)
+      lesson_time_array.push(attendance.lesson_status_07)
+      lesson_time_array.push(attendance.lesson_status_08)
+      lesson_time_array.push(attendance.lesson_status_09)
+      lesson_time_array.push(attendance.lesson_status_10)
+      lesson_time_array.push(attendance.lesson_status_11)
+      lesson_time_array.push(attendance.lesson_status_12)
+      lesson_time_array.push(attendance.lesson_status_13)
+      lesson_time_array.push(attendance.lesson_status_14)
+      lesson_time_array.push(attendance.lesson_status_15)
+      lesson_time_array.push(attendance.lesson_status_16)
+      lesson_time_array.push(attendance.lesson_status_17)
+      lesson_time_array.push(attendance.lesson_status_18)
+      lesson_time_array.push(attendance.lesson_status_19)
+      lesson_time_array.push(attendance.lesson_status_20)
+      lesson_time_array.push(attendance.lesson_status_21)
+      lesson_time_array.push(attendance.lesson_status_22)
+      lesson_time_array.push(attendance.lesson_status_23)
+    end
 
     # ボタンに表示する授業可能時間の最初の時間をfirst_timeに入れる
     first_time = lesson_time_array.find_index { |n| n == 1 }
 
-    # 授業可能時間のステータスが０時から23時分まで配列に入っているのでreverseして、その日の最後の授業可能時間を求める
+    # 授業可能時間のステータス（１）が０時から23時分まで配列に入っているのでreverseして、その日の最後の授業可能時間を求める
     lesson_time_array_reverse = lesson_time_array.reverse
     last_time = lesson_time_array_reverse.find_index { |n| n == 1 }
 
@@ -103,7 +133,7 @@ module ApplicationHelper
     elsif first_time == nil && last_time == nil
       return "Full"
     end
-    
+
     return "#{first_time}:00〜#{last_time}:59"
   end
   
